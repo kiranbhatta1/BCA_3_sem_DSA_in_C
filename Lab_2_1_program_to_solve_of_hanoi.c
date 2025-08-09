0478b9f1172fcad1fc5ee9bc6c6a8cac073fb66c
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-09 20:00:50 +0545

    Create Lab_2_1_program_to_solve_of_hanoi.c
    Write a program to solve the tower of hanoi.

---
+++ Lab_2_1_program_to_solve_of_hanoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void TOH(int n, char A, char B, char C);
+int main() {
+    int n;
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+    TOH(n, 'A', 'B', 'C');
+    return 0;
+}
+void TOH(int n, char A, char B, char C) {
+    if (n > 0) {
+        TOH(n - 1, A, C, B);
+        printf("Move disk %d from %c to %c\n", n, A, C);
+        TOH(n - 1, B, A, C);
+    }
+}
