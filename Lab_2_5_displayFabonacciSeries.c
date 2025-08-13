f96f06180b21134424665fe73dd0f4d7b228d805
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-08-13 07:14:38 +0545

    Create Lab_2_5_displayFabonacciSeries.c
    Write a program to display fibonacci series upto given number

---
+++ Lab_2_5_displayFabonacciSeries.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fib(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    int n, i;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("\nFibonacci numbers up to %d terms are:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("%d\t", fib(i));
+    }
+
+    return 0;
+}
